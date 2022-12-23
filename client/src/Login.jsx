0b99e4045764c27c6f036ec6d92e7ea50692412f
Craig Miller <craigmiller160@gmail.com>
2022-12-23 18:52:47 -0500

    Added commented code

--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -7,6 +7,21 @@ const initOptions = {
 
 const keycloak = Keycloak(initOptions);
 
+/*
+setInterval(() => {
+    keycloak.updateToken(70).then((refreshed) => {
+      if (refreshed) {
+        Vue.$log.info('Token refreshed' + refreshed);
+      } else {
+        Vue.$log.warn('Token not refreshed, valid for '
+          + Math.round(keycloak.tokenParsed.exp + keycloak.timeSkew - new Date().getTime() / 1000) + ' seconds');
+      }
+    }).catch(() => {
+      Vue.$log.error('Failed to refresh token');
+    });
+  }, 6000)
+ */
+
 export const Login = () => {
     // useEffect(() => {
     //     keycloak.init({ onLoad: initOptions.onLoad })
