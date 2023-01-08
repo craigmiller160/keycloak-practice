125faa65560318fd5c44e27df89cc10063f9fa1c
Craig Miller <craigmiller160@gmail.com>
2023-01-08 08:48:02 -0500

    Cleanup

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,8 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {Login} from "./Login.jsx";
-import {keycloakInit} from "./keycloakInit";
-import {UserInfo} from "./UserInfo";
 import {Root} from "./Root";
 
 const root = ReactDOM.createRoot(document.querySelector('#root'));
