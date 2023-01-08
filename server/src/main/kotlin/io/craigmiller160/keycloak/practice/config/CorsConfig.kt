2c3b338eb90a9ea0af1f03a3fdc0f514d43a8858
Craig Miller <craigmiller160@gmail.com>
2023-01-08 08:54:30 -0500

    Got the server to allow cors

---
+++ server/src/main/kotlin/io/craigmiller160/keycloak/practice/config/CorsConfig.kt
@@ -0,0 +1,16 @@
+package io.craigmiller160.keycloak.practice.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
+
+@Configuration
+class CorsConfig : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:3003")
+            .allowCredentials(true)
+            .allowedMethods("GET", "POST", "PUT", "DELETE")
+    }
+}
