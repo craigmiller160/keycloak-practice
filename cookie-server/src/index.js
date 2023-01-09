bb600c0ba46e7643284db27b79e63bbd89d17cdc
Craig Miller <craigmiller160@gmail.com>
2023-01-09 09:14:19 -0500

    Setup cookie server

--- cookie-server/src/index.js
+++ cookie-server/src/index.js
@@ -2,10 +2,29 @@ const express = require('express');
 
 const app = express();
 
+const createCookie = () => {
+    const value = 'HelloWorld; Max-Age=1000000'
+    return ['my-cookie', value];
+}
+
 app.get('/', (req, res) => {
+    const [name, value] = createCookie();
+    res.cookie(name, value);
     res.status(204);
     res.end();
 });
+
+app.get('/endpoint1', (req, res) => {
+    req.cookies.forEach((cookie) => console.log('COOKIE', cookie));
+    res.status(204);
+    res.end();
+})
+
+app.get('/endpoint2', (req, res) => {
+    req.cookies.forEach((cookie) => console.log('COOKIE', cookie));
+    res.status(204);
+    res.end();
+})
 
 app.listen(3001, () => {
     console.info('Listening');
