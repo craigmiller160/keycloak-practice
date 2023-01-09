0067d5fc591150189a9f5c389363fcf1639ac9ab
Craig Miller <craigmiller160@gmail.com>
2023-01-08 20:51:51 -0500

    Disabling java config

--- server/src/main/kotlin/io/craigmiller160/keycloak/practice/config/KeycloakConfig.kt
+++ server/src/main/kotlin/io/craigmiller160/keycloak/practice/config/KeycloakConfig.kt
@@ -5,9 +5,9 @@ import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
-@Configuration
-class KeycloakConfig {
-    @Bean
-    fun keycloakConfigResolver(): KeycloakConfigResolver =
-        KeycloakSpringBootConfigResolver()
-}
+//@Configuration
+//class KeycloakConfig {
+//    @Bean
+//    fun keycloakConfigResolver(): KeycloakConfigResolver =
+//        KeycloakSpringBootConfigResolver()
+//}
