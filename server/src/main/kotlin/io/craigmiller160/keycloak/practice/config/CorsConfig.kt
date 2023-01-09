e1789e545c93d643cf2e6142bbd26742b49fc8f9
Craig Miller <craigmiller160@gmail.com>
2023-01-08 21:19:33 -0500

    Can't figure out how to fix cors issue

--- server/src/main/kotlin/io/craigmiller160/keycloak/practice/config/CorsConfig.kt
+++ server/src/main/kotlin/io/craigmiller160/keycloak/practice/config/CorsConfig.kt
@@ -1,10 +1,12 @@
 package io.craigmiller160.keycloak.practice.config
 
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.annotation.Order
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
+@Order(Integer.MIN_VALUE)
 @Configuration
 class CorsConfig : WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
--- server/src/main/kotlin/io/craigmiller160/keycloak/practice/controller/HelloController.kt
+++ server/src/main/kotlin/io/craigmiller160/keycloak/practice/controller/HelloController.kt
@@ -1,8 +1,11 @@
 package io.craigmiller160.keycloak.practice.controller
 
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -13,4 +16,8 @@ class HelloController {
         println("AUTH HEADER: $authHeader")
         return "Hello World"
     }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun send(): String = ""
 }
